99d20508ad7ec67e2bd3ff9c457914abdb955b0f
Andrew Haupt & Kyle Mellander <student@epicodus.com>
2015-08-06 10:09:53 -0700

    Add test for dimes

--- spec/coin_combo_spec.rb
+++ spec/coin_combo_spec.rb
@@ -1,8 +1,14 @@
 require('rspec')
 require('coin_combo')
 
-describe('Float#coins') do
-  it("returns the correct amount of quarters") do
+describe('Numeric#coins') do
+
+  it("returns the correct amount of quarters") {
     expect((2.25).coins).to(eq"Your change is 9 quarters.")
-  end
+  }
+
+  it("returns the correct amount with dimes and quarters") {
+    expect((2.35).coins).to(eq("Your change is 9 quarters and 1 dime."))
+  }
+
 end
