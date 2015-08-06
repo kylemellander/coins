3c7e82457ec650ffdd0a9c25e27424056f606754
Andrew Haupt & Kyle Mellander <student@epicodus.com>
2015-08-06 12:09:32 -0700

    Add form for entering money total and display of result

--- app.rb
+++ app.rb
@@ -1,9 +1,13 @@
 require('sinatra')
 require('sinatra/reloader')
-require('./app')
 require('./lib/coin_combo')
-also_reload('./lib/**/*.rb')
+also_reload('lib/**/*.rb')
 
 get('/') {
   erb(:index)
+}
+
+get('/coins') {
+  @result = params.fetch("money").to_f.coins
+  erb(:coins)
 }
