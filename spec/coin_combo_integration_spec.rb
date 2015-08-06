49aaa4b7f1e848cd921049739b14b1a597706a47
Andrew Haupt & Kyle Mellander <student@epicodus.com>
2015-08-06 11:41:33 -0700

    Add test for inputting value and getting result in coin combo test

--- spec/coin_combo_integration_spec.rb
+++ spec/coin_combo_integration_spec.rb
@@ -8,4 +8,11 @@ describe('/', {:type => :feature}) {
     visit('/')
     expect(page).to have_content("Coin Combo!")
   }
+
+  it('displays result after coin input') {
+    visit('/')
+    fill_in('money', :with => '2.25')
+    click_button('Make Change For This')
+    expect(page).to have_content("Your change is 9 quarters.")
+  }
 }
